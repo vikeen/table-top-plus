b4eea0c2d40c1ab0f5b752adb781ab6d5679138c
John Rake <john.rake12@gmail.com>
2016-08-01 00:18:10 -0500

    add home page app

--- table_top_plus/settings.py
+++ table_top_plus/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'jquery',
     'bootstrap3',
 
-    'characters'
+    'characters',
+    'home'
 ]
 
 MIDDLEWARE_CLASSES = [
--- table_top_plus/urls.py
+++ table_top_plus/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from home import views
+
 
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^characters/', include('characters.urls')),
     url(r'^admin/', admin.site.urls),
 ]
